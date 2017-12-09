f2badfc4c3588ab70536da8290fd0030ada616a3
Lup <lapshenkov@gmail.com>
2017-12-10 01:13:18 +0300

    Написан таск для отправки байтов по USART0. Просто посылает байт 'a' постоянно и ждёт пока не закончится передача. Код написан, работоспособность не проверялась ни разу. Компилируется без ошибок. ЗЫ: надо почистить лишний код от демо и варнинги.

--- xmobile-fw/main.c
+++ xmobile-fw/main.c
@@ -153,61 +153,128 @@ void vApplicationIdleHook( void );
 
 static void vBlikGpio13Task( void* pvParameters );
 static void vBlikGpio7Task( void* pvParameters );
-
+inline void vTogglePin13();
+static void vUsartSendTask(void* pvParameters);
 /*-----------------------------------------------------------*/
+
+void vHwInitAtmega328p()
+{
+  /*UART 0*/
+  /* UCSR0A
+     |  7   |  6   |  5    |  4  |   3  |  2   |  1   |  0    |
+     | RXC0 | TXC0 | UDRE0 | FE0 | DOR0 | UPE0 | U2X0 | MPCM0 |
+     Bit7 - RXC0: USART receive complete flag. Is set when there is 
+                  unread data in buffer.
+     Bit6 - TXC0: USART transmit complete flag. Is set when a frame
+                  in shift register has been shifted out.
+     Bit5 - UDRE0:USART data register empty. Ready to receive new data.
+                  1 - buffer is empty
+     Bit4 - FE0:  frame error. (this bit received in Master mod)
+     Bit3 - DOR0: Data OverRun.
+     Bit2 - UPE0: USART Parity Error. Always set thi bit to zero 
+                  when writing to UCSR0A. (this bit received in Master 
+                  mod)
+     Bit1 - U2X0: Double the USART trabsmission speed. Only for async 
+                  operation. write to 0 when using synchronous 
+                  operation/
+     Bit0 - MPCM0: Multi-processor communication mode. */
+  
+
+  /* UCSR0B
+     |   7    |   6    |   5    |  4    |  3    |   2    |   1   |  0  |
+     | RXCIE0 | TXCIE0 | UDRIE0 | RXEN0 | TXEN0 | UCSZ02 | RXB80 |TXB80|
+     7 - RXCIE0: разрешение прерывания по завершению приёма.
+                 1 - приём завершён
+     6 - TXCIE0: разрешение прерываня по завершению передачи.
+     5 - UDRIE0: разрешение прерывания по опустошению регистра
+                 передатчика.
+     4 - RXEN0:  разрешение приёма. 
+                 1 - приём разрешён, переопределяется функционироване 
+                 вывода RXD
+     3 - TXEN0:  разрешенте передачи.
+     2 - UCSZ02: формат послок, сколько бит будет передавать в посылках 
+                 (5, 6, 7, 8 или 9)
+     1 - RXB80:  9й разряд принимаемых данных
+     0 - TXB80:  9й разряд передаваемых данных
+   */
+
+  
+  /* UCSR0C
+     |   7     |   6     |   5   |  4    |  3   |   2   |   1   |  0 |
+     | UMSEL01 | UMSEL00 | UPM01 | UPM00 | USBS0|UCSZ01/|UCSZ00/|UCPOL0|
+     |         |         |       |       |      |UDORD0 |UCPHA0 |
+     
+     7..6 - UMSEL0n: USART mode select.
+            UMSEL0[1:0] | mode
+	             00 | async USART
+		     01 | sync USART
+		     10 | reserved
+		     11 | master SPI
+     5..4 - USART parity mode. Режим проверки на чётность и нечётность
+     UPM[1:0] | mode
+           00 | Выключен
+	   10 | включён, проверка на чётность
+	   11 | включен, проверка на нечётность
+
+     3 - USBS: количество стоп битов.
+               1 - два стоп бита
+	       0 - один стоп бит
+	
+     2 - UCSZ01: character size
+  */
+
+  /* 
+     UBRR0L - скорость обмена данными 8bit used for boud rate
+     UBRR0H - скорость обмена данными 4bit register high [3-0] 
+              (наиболее значимые)
+     Для 16 мегагерц:
+     9600 bps: UBRRn = 103
+     1152k bps: UBRRn = 8
+     57.6  bps: UBRRn = 16
+*/
+  UBRR0H &= ~0x0F;
+  UBRR0L = 103; //9600 bps
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+  UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+}
 
 void main( void )
 {
- 
+  vHwInitAtmega328p();
   //prvIncrementResetCount();
-  {
-    unsigned char pin7 = (1 << 7);
-    int i = 0;
-    DDRD |= pin7;
-    PORTD |= pin7;
+  
+  vParTestInitialise();
 
-    for(;i < 10000; ++i)
-      {
-	PORTD ^= pin7;
-      }
-  }
-	/* Setup the LED's for output. */
-	vParTestInitialise();
+  xTaskCreate( vUsartSendTask,
+	       (signed char*)"send_uart__task",
+	       configMINIMAL_STACK_SIZE,
+	       NULL,
+	       1,
+	       NULL );
 
-	/* Create the standard demo tasks. */
-	//vStartIntegerMathTasks( tskIDLE_PRIORITY );
-	//vAltStartComTestTasks( mainCOM_TEST_PRIORITY, mainCOM_TEST_BAUD_RATE, mainCOM_TEST_LED );
-	//vStartPolledQueueTasks( mainQUEUE_POLL_PRIORITY );
-	//vStartRegTestTasks();
-
-	/* Create the tasks defined within this file. */
-	//xTaskCreate( vErrorChecks, "Check", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL );
-
-	/* Create the co-routines that flash the LED's. */
-	//vStartFlashCoRoutines( mainNUM_FLASH_COROUTINES );
-
-	/* In this port, to use preemptive scheduler define configUSE_PREEMPTION
-	as 1 in portmacro.h.  To use the cooperative scheduler define
-	configUSE_PREEMPTION as 0. */
-
-	xTaskCreate( vBlikGpio13Task,
-		     (signed char*)"blink_GPIO_13_task",
-		     configMINIMAL_STACK_SIZE,
-		     NULL,
-		     1,
-		     NULL );
-
-	xTaskCreate( vBlikGpio7Task,
-		     (signed char*)"blink_GPIO_7_task",
-		     configMINIMAL_STACK_SIZE,
-		     NULL,
-		     1,
-		     NULL );
+  xTaskCreate( vBlikGpio7Task,
+	       (signed char*)"blink_GPIO_7_task",
+	       configMINIMAL_STACK_SIZE,
+	       NULL,
+	       1,
+	       NULL );
 	
-	vTaskStartScheduler();
+  vTaskStartScheduler();
 
 }
 /*-----------------------------------------------------------*/
+
+static void vUsartSendTask(void* pvParameters)
+{
+  for(;;)
+    {
+      UDR0 = 'a';
+
+      while(UCSR0A & (1 << UDRE0) == 0) {}
+      
+      vTogglePin13();
+    }
+}
 
 static void vBlikGpio7Task( void* pvParameters )
 {
@@ -229,22 +296,10 @@ static void vBlikGpio7Task( void* pvParameters )
     }
 }
 
-static void vBlikGpio13Task( void* pvParameters )
+inline void vTogglePin13()
 {
-  //portB, pin5
-  unsigned char pin5 = (1 << 5);
-  unsigned char pin7 = (1 << 7);
-  
-  DDRB |= pin5;
-  PORTB |= pin5;
-  TickType_t xLastWakeTime;
-  
-  for(;;)
-    {
-      PORTB ^= pin5;
-      vTaskDelay( 250 );
-      vTaskDelayUntil(&xLastWakeTime, 250);
-    }
+  DDRB  |= (1 << 5);
+  PORTB ^= (1 << 5);
 }
 
 static void vErrorChecks( void *pvParameters )
